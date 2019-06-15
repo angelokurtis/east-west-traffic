e840e62bdc2cb9e62cccc4f3fe871a4cdb1cb43f
Tiago Angelo <tiago.angelo@sensedia.com>
2019-06-15 12:19:29 -0300

    Running the gRPC Server

--- server/src/main/kotlin/com/kurtis/server/ServerApplication.kt
+++ server/src/main/kotlin/com/kurtis/server/ServerApplication.kt
@@ -1,10 +1,28 @@
 package com.kurtis.server
 
+import io.grpc.ServerBuilder
+import mu.KotlinLogging
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+private const val PORT = 8080
+private val logger = KotlinLogging.logger {}
+
 @SpringBootApplication
-class ServerApplication
+class ServerApplication(val helloService: HelloService) : ApplicationRunner {
+	override fun run(args: ApplicationArguments) {
+		val server = ServerBuilder
+				.forPort(PORT)
+				.addService(helloService)
+				.build()
+		logger.info { "Running the gRPC Server on port $PORT" }
+
+		server.start()
+		server.awaitTermination()
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<ServerApplication>(*args)
